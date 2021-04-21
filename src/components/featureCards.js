e690d53a945abf394d4b142134c8039c47ddd0aa
Mikkel Hershøj Olsen <6107577@RTS-365.dk>
2021-04-21 10:59:59 +0200

    update

--- src/components/featureCards.js
+++ src/components/featureCards.js
@@ -6,14 +6,17 @@ export default function FeautureCard({ content }) {
                 if (item.type === "text") {
                     return <p className="itemText">{item.text}</p>
                 }
+    
                 
                 if (item.type === "headline") {
                     return <h1 className="itemHeadline">{item.headline}</h1>
                 }
+            
 
                 if (item.type === "image") {
                     return <img className="itemImage" src={item.url} alt=""/>
-                }           
+                }
+                return null           
             })}
         </div>
     )
